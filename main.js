827f3f1caa38257db1f0635fffa8ba22aa603794
RicardoGF396 <ricardogf396@gmail.com>
2021-09-22 18:44:50 -0500

    Primer commit

---
+++ main.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+    $(function(){
+        $('.bxslider').bxSlider({
+          mode: 'fade',
+          captions: true,
+          slideWidth: 1200
+        });
+      });
+
+      //Post
+  var posts = [
+
+    {
+      title: 'Titulo 1',
+      date: 'Publiado el día '+ moment().format('DD') + " de " + moment().format('MMMM') + ' del ' + moment().format('YYYY') ,
+      content:  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, cumque ratione aliquid nemo amet perspiciatis nihil, eos pariatur voluptatum suscipit totam quibusdam! Deleniti nostrum illo omnis sint, voluptates sequi! Quaerat aliquam quas voluptates quos quia sit laboriosam earum voluptatum. At quaerat sit voluptas explicabo delectus. Quasi sequi nemo officia? Pariatur velit repellat voluptatibus quas unde. Nesciunt tempora, recusandae quos voluptatem maxime, ex praesentium officia ab odio consequuntur facilis optio harum vitae cum est, numquam id eligendi culpa veniam vero assumenda. Nam suscipit incidunt praesentium beatae ab id deserunt pariatur, rerum accusantium nemo. Soluta doloremque expedita sed error. Quasi, voluptas amet?'
+    },{
+      title: 'Titulo 2',
+      date: 'Publiado el día '+ moment().format('DD') + " de " + moment().format('MMMM') + ' del ' + moment().format('YYYY') ,
+      content:  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, cumque ratione aliquid nemo amet perspiciatis nihil, eos pariatur voluptatum suscipit totam quibusdam! Deleniti nostrum illo omnis sint, voluptates sequi! Quaerat aliquam quas voluptates quos quia sit laboriosam earum voluptatum. At quaerat sit voluptas explicabo delectus. Quasi sequi nemo officia? Pariatur velit repellat voluptatibus quas unde. Nesciunt tempora, recusandae quos voluptatem maxime, ex praesentium officia ab odio consequuntur facilis optio harum vitae cum est, numquam id eligendi culpa veniam vero assumenda. Nam suscipit incidunt praesentium beatae ab id deserunt pariatur, rerum accusantium nemo. Soluta doloremque expedita sed error. Quasi, voluptas amet?'
+    },
+
+  ];
+
+  console.log(posts);
+
+  //Recorrer el JSON para enviar al index.html
+
+  posts.forEach((item,index) =>{
+    var post = `<article class="post">
+                  <h2>${item.title} </h2>
+                  <span class="date">${item.date} </span>
+                  <p>
+                  ${item.content}
+              </p>
+              <div class="btn-link"><a href="#" class="button-more">Artículo completo</a></div>
+            </article>
+            
+            `;
+
+          //Encontrar el div con ID posts y meterle el JSO
+            $('#posts').append(post);
+  });
+
+
+  //Selector de tema
+
+    var theme = $("#theme");
+    
+    $("#to-red").click(function(){
+      theme.attr("href","red.css")
+      alert('Esta wea cambio de tema a Rojo')
+    })
+
+    $("#to-green").click(function(){
+      theme.attr("href","green.css")
+      alert('Esta wea cambio de tema a Verde')
+    })
+
+    $("#to-blue").click(function(){
+      theme.attr("href","blue.css")
+      alert('Esta wea cambio de tema a Azul')
+    })
+
+  });
+
